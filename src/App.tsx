2320e982c3cc7728aaa1f78bb0a022bf103a8f57
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-12-20 19:53:41 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// LAYOUTS
+import MainLayout from "~/layouts/MainLayout";
+// COMPONENTS
+import UserTable from "./components/UserTable";
+
+function App() {
+	return (
+		<MainLayout>
+			<UserTable />
+		</MainLayout>
+	);
+}
+
+export default App;
