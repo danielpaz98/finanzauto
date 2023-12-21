3b7ab9506fbee9007a78c0dc0207d56621cd0ffb
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-12-21 10:40:06 -0500

    update: remove dark class

--- src/components/UserModal/index.tsx
+++ src/components/UserModal/index.tsx
@@ -74,7 +74,7 @@ export default function UserModal({ show, user, mode, onClose }: Props) {
 					)}
 
 					<button
-						className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+						className="ms-3 focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600"
 						type="button"
 						onClick={handleOnCloseModal}
 					>
